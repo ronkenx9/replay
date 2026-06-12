eb3ef40e63ff4818d3c8501ea3dd48ec2fa8b382
Roninxx <gadgetplug@Ronin-MacBook-Air.local>
2026-06-12 15:23:01 +0100

    fix: exclude dist directory from vitest tests

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    exclude: ["**/node_modules/**", "**/dist/**", "**/cypress/**", "**/.{idea,git,cache,output,temp}/**"],
   },
 });
